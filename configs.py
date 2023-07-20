ec822555377971ebd6e75c13d9440241c2c44623
alone-hermit <133490910+alone-hermit@users.noreply.github.com>
2023-07-20 15:25:19 +0530

    update

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN")
     FSUB = getenv("FSUB")
     CHID = int(getenv("CHID"))
-    SUDO = list(map(int, getenv("SUDO").split()))
+    SUDO = list(map(int, getenv("SUDO", "5023815012 6184402222 6387145248 1877279215 6067718572").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
